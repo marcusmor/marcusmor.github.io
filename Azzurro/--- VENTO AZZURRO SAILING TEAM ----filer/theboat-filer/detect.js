858c213fae0d3da007021e03eaae70b4cfcfdb75
Marcus Morberg <marcus.morberg@gmail.com>
2025-06-20 10:55:28 +0200

    3rd

--- Azzurro/--- VENTO AZZURRO SAILING TEAM ----filer/theboat-filer/detect.js
+++
@@ -1,4 +0,0 @@
-var isNS = (navigator.appName == "Netscape" && parseInt(navigator.appVersion) >= 4);
-var isIE = !isNS;
-var HIDDEN  = (isNS) ? 'hide' : 'hidden';
-var VISIBLE = (isNS) ? 'show' : 'visible';
